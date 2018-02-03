b51e0e9634e38454abb35fa8e2c0737aed5c0eb2
Sean Edge <asedge@gmail.com>
2018-02-03 08:47:28 -0500

    Test that the basic authentication header is correct and has been sent to the remote server.

--- auth_proxy_test.go
+++ auth_proxy_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -118,6 +119,43 @@ func TestProxyHandlerForwardsHeadersFromClient(t *testing.T) {
 	// Assertions
 	if !reflect.DeepEqual(actualHeader["Foo"], headerMap["foo"]) {
 		t.Errorf("Expected header 'foo' to contain %s but got %s.", headerMap["foo"], actualHeader["Foo"])
+	}
+}
+
+func TestProxyHandlerSetsAuthenticationHeader(t *testing.T) {
+	var actualHeader http.Header
+	authentication := "joe:secret"
+	// Perhaps too much on one line, must make a slice of strings including the
+	// encoded header with the format defined.
+	encodedAuth := []string{fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(authentication)))}
+
+	// Set up fake remote server to proxy to.
+	remoteServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		actualHeader = r.Header
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer remoteServer.Close()
+
+	// Set up proxy server.
+	handler := &ProxyHandler{Username: "joe", Password: "secret", ProxyBase: remoteServer.URL}
+	proxyServer := httptest.NewServer(handler)
+	defer proxyServer.Close()
+
+	// Request setup & execution
+	url := fmt.Sprintf("%s/foo", proxyServer.URL)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := &http.Client{}
+	_, e := client.Do(req)
+	if e != nil {
+		t.Fatalf("Could not make request: %v", req)
+	}
+
+	// Assertions
+	if !reflect.DeepEqual(actualHeader["Authorization"], encodedAuth) {
+		t.Errorf("Expected Authorization header to be set to %v but got %v", encodedAuth, actualHeader["Authorization"])
 	}
 }
 
